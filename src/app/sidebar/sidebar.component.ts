a16e6d5713a31477a239197461fd0a1628af4948
Lyssa <lyssa.lima@poli.digital>
2023-10-28 18:15:41 -0300

    feat: adding sidebar component

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  icon_sidebar: string = 'assets/images/icon-sidebar.png';
+  ellipse_photo: string = 'assets/images/ellipse-profile.png';
+}
